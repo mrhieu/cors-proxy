e8a09de21cf066582b34792db83486eb910cac5c
Hieu Pham <mr_hie@yahoo.com>
2018-01-16 03:02:26 +0800

    fix: use targetURL alone

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ app.all('*', function (req, res, next) {
 
   // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+  // res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+  res.header("Access-Control-Allow-Methods", "GET");
   res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
 
   if (req.method === 'OPTIONS') {
@@ -20,20 +21,20 @@ app.all('*', function (req, res, next) {
     res.send();
   } else {
     var targetURL = req.header('Target-URL');
+
     if (!targetURL) {
       res.send(500, { error: 'There is no Target-URL header in the request' });
       return;
     }
-    
+
     request({
-      url: targetURL + req.url,
+      url: targetURL,
       method: req.method,
       json: req.body,
       headers: {'Authorization': req.header('Authorization') || null}
-    },
-    function (error, response, body) {
+    }, function (error, response, body) {
       if (error) {
-        console.error('error: ' + response.statusCode)
+        console.error('error: ' + error)
       }
     }).pipe(res);
   }
